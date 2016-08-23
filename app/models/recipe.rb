7f761e3d8e27d5ac4fa14b4673d7745c3d25cc56
Daniel Uribe <duribe8@gmail.com>
2016-08-22 21:42:32 -0700

    Added forms for Ingredients and Steps

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -12,4 +12,5 @@ class Recipe < ActiveRecord::Base
   validates :title, :description, :image, presence: true                              
   has_attached_file :image, styles: { medium: "400x400>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+  
 end
